b49bbb8107648be19fc36ecd33633549f3db8f4d
Moroz Roman <morozdef24@gmail.com>
2025-03-12 22:06:06 +0300

    lifespan

--- app/main.py
+++ app/main.py
@@ -1,10 +1,23 @@
 import uvicorn
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
 
 from api import router as api_router
 from core.config import settings
+from core.models import db_helper
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # startup
+    yield
+    #shutdown
+    await db_helper.dispose()
+
+
+
+app = FastAPI(
+    lifespan=lifespan,
+)
 
 app.include_router(
     api_router,
