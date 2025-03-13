569a21e6ca61d8577a53033d88b4b90f278eb0d1
Moroz Roman <morozdef24@gmail.com>
2025-03-13 19:15:41 +0300

    make base

--- app/core/models/base.py
+++ app/core/models/base.py
@@ -1,8 +1,13 @@
-from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import DeclarativeBase, declared_attr
 from sqlalchemy.orm import Mapped, mapped_column 
 
 class Base(DeclarativeBase):
     __abstract__ = True
+    
+    @declared_attr
+    def __tablename__(cls) -> str:
+        return cls.__name__.lower() 
+    
     id: Mapped[int] = mapped_column(primary_key=True)
     
 class User(Base):
