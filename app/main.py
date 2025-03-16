39516ac0fd27a791d0ca4a053a12b397f9c2a6cd
Moroz Roman <morozdef24@gmail.com>
2025-03-16 21:10:58 +0300

    poetry

--- app/main.py
+++ app/main.py
@@ -9,7 +9,7 @@ from core.models.base import Base
 
 @asynccontextmanager
 async def lifespan(app_: FastAPI):
-    # startup\
+    # startup
     async with db_helper.engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
     yield
