a1243fc710bd306a16da772fcf75376a83ba537f
Moroz Roman <morozdef24@gmail.com>
2025-03-16 22:09:40 +0300

    new cols create migration

---
+++ app/alembic/versions/2025_03_16_2206-45dc3840c591_create_cols_in_users_table.py
@@ -0,0 +1,71 @@
+"""create cols in users table
+
+Revision ID: 45dc3840c591
+Revises: 0c2735e78bb7
+Create Date: 2025-03-16 22:06:31.725317
+
+"""
+
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = "45dc3840c591"
+down_revision: Union[str, None] = "0c2735e78bb7"
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    """Upgrade schema."""
+    op.add_column("users", sa.Column("email", sa.String(), nullable=False))
+    op.add_column(
+        "users", sa.Column("is_active", sa.Boolean(), nullable=False)
+    )
+    op.add_column(
+        "users", sa.Column("is_superuser", sa.Boolean(), nullable=False)
+    )
+    op.add_column(
+        "users", sa.Column("last_login_at", sa.DateTime(), nullable=True)
+    )
+    op.add_column(
+        "users", sa.Column("created_at", sa.DateTime(), nullable=False)
+    )
+    op.create_index(
+        "idx_users_created_at", "users", ["created_at"], unique=False
+    )
+    op.create_index("idx_users_email", "users", ["email"], unique=False)
+    op.create_index(
+        "idx_users_is_active", "users", ["is_active"], unique=False
+    )
+    op.create_index(
+        "idx_users_is_superuser", "users", ["is_superuser"], unique=False
+    )
+    op.create_index(
+        "idx_users_last_login_at", "users", ["last_login_at"], unique=False
+    )
+    op.create_index("idx_users_username", "users", ["username"], unique=False)
+    op.create_unique_constraint(op.f("uq_users_email"), "users", ["email"])
+    op.create_unique_constraint(
+        "uq_users_username_email", "users", ["username", "email"]
+    )
+
+
+def downgrade() -> None:
+    """Downgrade schema."""
+    op.drop_constraint("uq_users_username_email", "users", type_="unique")
+    op.drop_constraint(op.f("uq_users_email"), "users", type_="unique")
+    op.drop_index("idx_users_username", table_name="users")
+    op.drop_index("idx_users_last_login_at", table_name="users")
+    op.drop_index("idx_users_is_superuser", table_name="users")
+    op.drop_index("idx_users_is_active", table_name="users")
+    op.drop_index("idx_users_email", table_name="users")
+    op.drop_index("idx_users_created_at", table_name="users")
+    op.drop_column("users", "created_at")
+    op.drop_column("users", "last_login_at")
+    op.drop_column("users", "is_superuser")
+    op.drop_column("users", "is_active")
+    op.drop_column("users", "email")
--- app/core/models/user.py
+++ app/core/models/user.py
@@ -6,21 +6,21 @@ from sqlalchemy.orm import Mapped, mapped_column
 
 class User(Base):
     username: Mapped[str] = mapped_column(unique=True)
-    # email: Mapped[str] = mapped_column(unique=True)
-    # is_active: Mapped[bool] = mapped_column(default=True)
-    # is_superuser: Mapped[bool] = mapped_column(default=False)
-    # last_login_at: Mapped[datetime] = mapped_column(nullable=True)
-    # created_at: Mapped[datetime] = mapped_column(default=datetime.now())
+    email: Mapped[str] = mapped_column(unique=True)
+    is_active: Mapped[bool] = mapped_column(default=True)
+    is_superuser: Mapped[bool] = mapped_column(default=False)
+    last_login_at: Mapped[datetime] = mapped_column(nullable=True)
+    created_at: Mapped[datetime] = mapped_column(default=datetime.now())
     
-    # __table_args__ = (
-    #     Index('idx_users_username', 'username'),
-    #     Index('idx_users_email', 'email'),
-    #     Index('idx_users_is_active', 'is_active'),
-    #     Index('idx_users_is_superuser', 'is_superuser'),
-    #     Index('idx_users_last_login_at', 'last_login_at'),
-    #     Index('idx_users_created_at', 'created_at'),
-    #     CheckConstraint('is_active IN (0, 1)', name='ck_users_is_active'),
-    #     CheckConstraint('is_superuser IN (0, 1)', name='ck_users_is_superuser'),
-    #     UniqueConstraint('username', 'email', name='uq_users_username_email'),
-    # )
+    __table_args__ = (
+        Index('idx_users_username', 'username'),
+        Index('idx_users_email', 'email'),
+        Index('idx_users_is_active', 'is_active'),
+        Index('idx_users_is_superuser', 'is_superuser'),
+        Index('idx_users_last_login_at', 'last_login_at'),
+        Index('idx_users_created_at', 'created_at'),
+        CheckConstraint('is_active IN (0, 1)', name='ck_users_is_active'),
+        CheckConstraint('is_superuser IN (0, 1)', name='ck_users_is_superuser'),
+        UniqueConstraint('username', 'email', name='uq_users_username_email'),
+    )
     
